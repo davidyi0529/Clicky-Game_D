2b3d01d46587b304f4beff54584215310428b896
David Yi <david.yi.0529@gmail.com>
2020-11-10 20:48:34 -0800

    inital commit npx create

---
+++ simpsons-memory-game_d/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends: friends
+  };
+
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
