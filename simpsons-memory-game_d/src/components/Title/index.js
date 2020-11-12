ea04c75f469b62a5d4e8329327de62f906916489
David Yi <david.yi.0529@gmail.com>
2020-11-11 22:13:35 -0800

    fixed title header

--- simpsons-memory-game_d/src/components/Title/index.js
+++ simpsons-memory-game_d/src/components/Title/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const Title = props => (
   <div className="title">
-    <h1><img className='simpsons' src='/img/simpsonslogo.png' alt='Simpsons'/></h1>
+    <h1><img className='simpsons' src='https://github.com/davidyi0529/Simpsons-Memory-Game_D/blob/main/simpsons-memory-game_d/public/img/simpsonslogo.png?raw=true' alt='Simpsons'/></h1>
     Score: <span className="score">{props.correctGuesses} &nbsp; &nbsp;| &nbsp; &nbsp;</span>
     Top Score: <span className="score">{props.bestScore}</span>
   </div>
